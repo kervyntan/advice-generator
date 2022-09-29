849c0fe83769ed0e8f14d39a0538daa990be67f0
iamkervyntan <kervyntan@gmail.com>
2022-09-29 15:09:36 +0800

    added the fetch call for the advice API

---
+++ assets/main.js
@@ -0,0 +1,15 @@
+const advice = document.querySelector(".advice-text");
+
+fetch("https://api.adviceslip.com/advice")
+.then((res) => 
+// cannot have curly braces here since the content will stay in this scope
+// cannot pass the json into the next promise
+    res.json()
+)
+.then((data) => {
+    advice.innerHTML = data.slip.advice;
+    // console.log(data.slip.advice);
+})
+
+
+
